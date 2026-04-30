b071ed89be4b92892ba006fdf8e41482344ec2b3
Your Name <you@example.com>
2026-04-30 18:07:46 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -49,7 +49,7 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2))
     map->root = NULL;
     map->current = NULL;
     map->lower_than = lower_than;
-    return;
+    return map;
 }
 
 // 2. Implemente la función Pair* searchTreeMap(TreeMap* tree, void* key), 
