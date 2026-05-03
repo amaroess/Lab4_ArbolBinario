5e73fcc53dbebf6f1dea3d6298fd337436ff4e42
Your Name <you@example.com>
2026-05-03 15:33:52 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -135,7 +135,7 @@ TreeNode * minimum(TreeNode * x)
 
 void removeNode(TreeMap * tree, TreeNode* node) 
 {
-    if(tree == NULL || tree->root == NULL || node == NULL) return; 
+    if(tree == NULL || tree->root == NULL) return; 
     if(node->left != NULL  && node->right != NULL) 
     {
         TreeNode* sucesor = node->right; 
